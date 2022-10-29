4da7e20c70b74cf87c70a03b7996ab5e2cca538a
Kelly Miranda <klmiranda0703.kelly@gmail.com>
2022-10-29 16:32:50 -0500

    feat: agregado endpoint de consulta de crear user

--- src/user/controllers/auth.controller.ts
+++ src/user/controllers/auth.controller.ts
@@ -4,11 +4,18 @@ import { LoginDto } from '../dto/login-user.dto';
 import { AuthService } from '../providers/auth.service';
 import { TokenResponseDto } from '../dto/token.dto';
 import { ChangePasswordDto } from '../dto/change-password.dto';
+import { UserService } from '../providers/user.service';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UserDto } from '../dto/user.dto';
+
 @ApiHeader({ name: 'Accept-Language', description: 'Lang' })
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post('login')
   @ApiCreatedResponse({ type: TokenResponseDto })
@@ -26,5 +33,11 @@ export class AuthController {
   @ApiCreatedResponse({ type: Boolean })
   async changePassword(@Body() payload: ChangePasswordDto): Promise<boolean> {
     return await this.authService.changePassword(payload);
+  }
+
+  @Post('sing-up')
+  @ApiCreatedResponse({ type: UserDto })
+  async signUp(@Body() payload: CreateUserDto): Promise<UserDto> {
+    return await this.userService.createUser(payload);
   }
 }
